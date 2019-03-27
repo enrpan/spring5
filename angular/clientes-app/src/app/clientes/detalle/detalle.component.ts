95b001afed40c41d1915d41dc79e53a657d53004
enrpan <epsbgt@gmail.com>
2019-03-27 23:50:09 +0100

    20190327 Clase 95

--- angular/clientes-app/src/app/clientes/detalle/detalle.component.ts
+++ angular/clientes-app/src/app/clientes/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'detalle-cliente',
@@ -11,18 +12,34 @@ import { ActivatedRoute } from '@angular/router';
 export class DetalleComponent implements OnInit {
   cliente: Cliente;
   titulo: string = "Detalle del cliente";
+  private fotoSeleccionada: File;
 
   constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.paramMap.subscribe(params => {
       let id:number = +params.get('id');  // El + es para convertir a numero
       if ( id ) {
         this.clienteService.getCliente(id).subscribe(cliente => {
           this.cliente = cliente;
-        })
+        });
       }
-    })
+    });
+  }
+
+  seleccionarFoto(event) {
+    this.fotoSeleccionada = event.target.files[0];
+    console.log(this.fotoSeleccionada);
+  }
+
+  subirFoto() {
+    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
+    .subscribe( cliente => {
+      this.cliente = cliente;
+      swal.fire('La foto se ha subido completamente!', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
+    }
+
+    );
   }
 
 }
