d7dc848ac872e93f89ae29fba9d50f52d952656b
enrpan <epsbgt@gmail.com>
2019-05-02 23:30:56 +0200

    20190502 Clase 142

--- angular/clientes-app/src/app/usuarios/login.component.ts
+++ angular/clientes-app/src/app/usuarios/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from './usuario';
 import swal from 'sweetalert2';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,15 @@ export class LoginComponent implements OnInit {
   titulo:string = 'Por favor Sign In!';
   usuario: Usuario;
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
     this.usuario = new Usuario();
   }
 
   ngOnInit() {
+    if ( this.authService.isAuthenticated() ) {
+      swal.fire('Login', `Hola ${this.authService.usuario.username} ya estás autenticado!`, 'info');
+      this.router.navigate(['/clientes']);
+    }
   }
 
   login(): void {
@@ -24,6 +30,22 @@ export class LoginComponent implements OnInit {
       swal.fire('Error Login', 'Username o password vacías!', 'error');
       return;
     }
+
+    this.authService.login(this.usuario).subscribe(response => {
+      console.log(response);
+
+      this.authService.guardarUsuario(response.access_token);
+      this.authService.guardarToken(response.access_token);
+
+      let usuario = this.authService.usuario;  // Aunque se usa como si fuera un variable de la clase, en realidad se llama al metodo get definido
+
+      this.router.navigate(['/clientes']);
+      swal.fire('Login', `Hola ${usuario.username}, has iniciado sesión con éxito!`, 'success');
+    }, err => {
+      if ( err.status==400 ) {  // error de autenticación
+        swal.fire('Error Login', 'Username o password incorrectos!', 'error');
+      }
+    });
   }
 
 }
