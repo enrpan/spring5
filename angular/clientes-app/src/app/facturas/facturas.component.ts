f886e90d5b2b775d81a608e977ec2c1e5bedc1d7
enrpan <epsbgt@gmail.com>
2019-05-23 23:35:58 +0200

    20190523 Clase 179

--- angular/clientes-app/src/app/facturas/facturas.component.ts
+++ angular/clientes-app/src/app/facturas/facturas.component.ts
@@ -8,6 +8,8 @@ import {Observable} from 'rxjs';
 import {map, flatMap} from 'rxjs/operators';
 import { FacturaService } from './services/factura.service';
 import { Producto } from './models/producto';
+import { ItemFactura } from './models/item-factura';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
 
 @Component({
   selector: 'app-facturas',
@@ -48,6 +50,61 @@ export class FacturasComponent implements OnInit {
   // El string | undefined como valor de retorno indica que lo retornado puede ser una cadena o undefined
   mostrarNombre(producto?: Producto):string | undefined {
     return producto? producto.nombre : undefined;
+  }
+
+  seleccionarProducto(event: MatAutocompleteSelectedEvent): void {
+    let producto = event.option.value as Producto;
+    console.log(producto);
+
+    if ( this.existeItem(producto.id) ) {
+      this.incrementaCantidad(producto.id);
+    } else {
+      let nuevoItem = new ItemFactura();
+      nuevoItem.producto = producto;
+      this.factura.items.push(nuevoItem);
+    }
+
+    this.autocompleteControl.setValue('');
+    event.option.focus();
+    event.option.deselect();
+  }
+
+  actualizarCantidad(id: number, event: any):void {
+    let cantidad: number = event.target.value as number;
+
+    if ( cantidad==0 ) {
+      return this.eliminarItemFactura(id);
+    }
+
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if ( id===item.producto.id ) {
+        item.cantidad = cantidad;
+      }
+      return item;
+    });
+  }
+
+  existeItem(id:number): boolean {
+    let existe = false;
+    this.factura.items.forEach((item:ItemFactura) => {
+      if ( id===item.producto.id ) {
+        existe = true;
+      }
+    });
+    return existe;
+  }
+
+  incrementaCantidad(id:number): void {
+    this.factura.items = this.factura.items.map((item:ItemFactura) => {
+      if ( id===item.producto.id ) {
+        ++item.cantidad;
+      }
+      return item;
+    });
+  }
+
+  eliminarItemFactura(id:number): void {
+    this.factura.items = this.factura.items.filter((item: ItemFactura) => id !== item.producto.id);
   }
 
 }
